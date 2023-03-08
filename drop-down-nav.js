406fe409881d7521433d029bc159469bed68d452
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2023-03-08 10:27:39 -0500

    adds js file

---
+++ drop-down-nav.js
@@ -0,0 +1,9 @@
+// const mobileMenuSlider = document.querySelector(".menu-icon");
+// const closeMenuSlider = document.querySelector(".close-menu-slider");
+
+// mobileMenuSlider.addEventListener("click", function () {
+//   mobileMenuSlider.classList.toggle("close-menu-slider");
+// });
+// closeMenuSlider.addEventListener("click", function () {
+//   closeMenuSlider.classList.toggle();
+// });
